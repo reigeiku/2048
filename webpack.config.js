9d3fef3be1de284266bc99dbc2a2690f8bf05e8d
Jeric <fajardo.jeric@yahoo.com>
2022-10-02 00:16:35 +0100

    fix(config): fix errors in config

--- webpack.config.js
+++ webpack.config.js
@@ -3,66 +3,77 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const path = require("path");
 
-module.exports = function build(env, arg) {
-    const config = {
-        entry: "./src/index.js",
-        output: {
-            filename: "main.js",
-            path: path.resolve(__dirname, "dist"),
-        },
-        devServer: {
-            contentBase: path.join(__dirname, "dist"),
-            compress: true,
-            port: 3001,
-            writeToDisk: true,
-        },
-        mode: arg.mode,
-        devtool: "source-map",
-        module: {
-            rules: [
-                {
-                    test: /\/(ts|js)x$/,
-                    exclude: /nodule_modules/,
-                    loader: "babel-loader",
-                },
-                {
-                    test: /\.css$/,
-                    include: path.resolve(__dirnamem, "public"),
-                    exclude: /nodule_modules/,
-                    use: [
-                        {
-                            loader: MiniCssExtractPlugin.loader,
+module.exports = {
+    entry: [
+        "./src/Tile.ts",
+        "./src/brain.ts",
+        "./src/heart.ts",
+        "./src/limbs.ts",
+    ],
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+        compress: true,
+        port: 3001,
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\/(ts|js)x$/,
+                exclude: /nodule_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                "@babel/preset-env",
+                                "@babel/preset-typescript",
+                            ],
                         },
-                        {
-                            loader: "css-loader",
-                            options: {
-                                importLoaders: 1,
-                            },
-                        },
-                    ],
-                },
-            ],
-            plugins: [
-                new MiniCssExtractPlugin({
-                    filename: "[name].bundle.css",
-                    chunkFilename: "[id].css",
-                }),
-
-                new HtmlWebpackPlugin({
-                    template: "./public/index.html",
-                    filename: "index.html",
-                }),
-            ],
-            resolve: {
-                modules: [
-                    path.resolve(__dirname, "/src"),
-                    path.resolve(__dirname, "/public"),
-                    path.resolve(__dirname, "node_modules/"),
+                    },
+                    {
+                        loader: "ts-loader",
+                    },
                 ],
-                extentions: [".ts", ".js", ".css"],
             },
-        },
-    };
+            {
+                test: /\.css$/,
+                include: path.resolve(__dirname, "public"),
+                exclude: /nodule_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].bundle.css",
+            chunkFilename: "[id].css",
+        }),
 
-    return config;
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "index.html",
+        }),
+    ],
+    resolve: {
+        modules: [
+            path.resolve(__dirname, "/src"),
+            path.resolve(__dirname, "/public"),
+            path.resolve(__dirname, "node_modules/"),
+        ],
+        extensions: [".ts", ".js", ".css"],
+    },
 };
